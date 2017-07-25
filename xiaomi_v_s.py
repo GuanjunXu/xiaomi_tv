1a60c38fdec54d9e302d6a5cb1e566db6f4d2003
GuanjunXu <344517544@qq.com>
2017-07-25 17:48:32 +0800

    update

--- xiaomi_v_s.py
+++ xiaomi_v_s.py
@@ -6,7 +6,9 @@ import time
 import xlrd
 import xlwt
 
-tv_d, controller_d, reader_d = "10.58.108.189:5555", "9cf32d64", "910019c9"
+# os.popen('CHCP 65001')
+
+tv_d, controller_d, reader_d = "192.168.199.115:5555", "192.168.199.216:5555", "192.168.199.125:5555"
 
 tv = Device(tv_d)
 controller = Device(controller_d)
@@ -15,17 +17,22 @@ reader = Device(reader_d)
 reader_activity = "com.kingfore.TextToVoice/com.iflytek.common.LaunchService"
 controller_activity = "com.duokan.phone.remotecontroller/com.xiaomi.mitv.phone.remotecontroller.home.MainActivity"
 
-f_query = open("query_list.txt", "r")
+f_query = open("query_list_2.txt", "r")
 query = f_query.readline()
 
 wb = xlwt.Workbook()
-ws = wb.add_sheet('Xiaomi search result')
+ws = wb.add_sheet('Xiaomi search result', cell_overwrite_ok = True)
 
-col_names = ["Query", "Recognition", "ResultCount", "Results", "ImgPath"]
+col_names = ["Query", "Recognition", "ResultCount", "Results", "ImgPath", "ResultMsg"]
 
 work_path = os.getcwd()
+result_path = "Results"
+try:
+    os.mkdir(result_path)
+except:
+    pass
 
-def longpressDown(hold):
+def longpressDown():
     os.popen("adb -s %s shell sendevent /dev/input/event2 3 57 50"%controller_d)
     os.popen("adb -s %s shell sendevent /dev/input/event2 3 53 1135"%controller_d)
     os.popen("adb -s %s shell sendevent /dev/input/event2 3 54 2436"%controller_d)
@@ -50,17 +57,19 @@ def launchReader():
         reader(text = u'文字转语音合成器').click()
         time.sleep(5)
 
+controller.watcher("gototv").when(text=u"客厅的小米电视").click(text=u"客厅的小米电视")
+
 def launchController():
-    if controller(packageName = 'com.duokan.phone.remotecontroller').wait.gone():
+    if controller(packageName = 'com.duokan.phone.remotecontroller', text = u'开关').wait.gone():
         # os.popen("adb -s %s shell am start -n %s"%(controller_d, controller_activity))
         controller.press('home')
         time.sleep(2)
         controller.press('home')
         controller(text = u'万能遥控').click()
-        try:
+        time.sleep(2)
+        if controller(text = u'开关').wait.gone():
             controller(text = u'客厅的小米电视').click()
-        except:
-            pass
+            time.sleep(2)
 
 def findText(txt):
     before = s.index(u"找到")
@@ -71,42 +80,51 @@ def findText(txt):
 
 def screencapAndPullOut(imgname, pullpath):
     imgname = str(imgname)
-    os.popen("adb shell screencap -p /sdcard/%s.png"%imgname)
-    os.popen("adb pull /sdcard/%s.png %s"%(imgname, pullpath))
-    os.popen("adb shell rm /sdcard/%s.png"%imgname)
+    os.popen("adb -s %s shell screencap -p /sdcard/%s.png"%(tv_d,imgname))
+    os.popen("adb -s %s pull /sdcard/%s.png %s"%(tv_d,imgname, pullpath))
+    os.popen("adb -s %s shell rm /sdcard/%s.png"%(tv_d,imgname))
 
-for m in range(len(col_names)):
-    ws.write(0, m, col_names[m])
-
-i = 1
 while query:
-    line_w = []
-    query = query.strip("\n")
-    line_w.append(query)
+    q = query.strip("\n").split(',')
+    no = q[0]
+    word = q[1]
     try:
         launchReader()
-        reader(resourceId = 'com.kingfore.TextToVoice:id/mainEditText1').set_text(query) # 朗读机输入文字
+        reader(resourceId = 'com.kingfore.TextToVoice:id/mainEditText1').set_text(word) # 朗读机输入文字
         launchController()
+        print "\tPress down... ",
         longpressDown() # 遥控器按住语音键
-        reader(resourceId = '朗读').click() # 朗读机按下朗读键
-        longpressUp(len(query)/2) # 遥控器释放语音键
-        result_txtTitle = tv(resourceId = 'com.xiaomi.voicecontrol:id/txtTitle').info['text']
-        find_list = findText(result_txtTitle)
-        res_count, rec = find_list[0], find_list[1]
-        line_w.append(res_count)
-        line_w.append(rec)
-        result_count = tv(resourceId = 'com.xiaomi.voicecontrol:id/poster').count
-        res_tit_list = []
-        for t in range(result_count):
-            res_tit = tv(resourceId = 'com.xiaomi.voicecontrol:id/poster', instance = t).down(resourceId = 'com.xiaomi.voicecontrol:id/title').info['text']
-            res_tit_list.append(res_tit)
-        line_w.append(res_tit_list)
-        line_w.append(work_path + str(i) + ".png")
-        for n in range(len(line_w)):
-            ws.write(i, n, line_w(n))
-        screencapAndPullOut(i, work_path)
-        query = f_query.readline()
+        print "\tReading... ",
+        reader(text = u'朗读').click() # 朗读机按下朗读键
+        print "\tRelease... ",
+        longpressUp(len(word)/2) # 遥控器释放语音键
+        # if tv(resourceId = 'com.mitv.tvhome:id/collect_btn').wait.exists():
+        #     res_tit = tv(resourceId = 'com.mitv.tvhome:id/name').text
+        #     dict_w['title'] = res_tit
+        #     dict_w['tag'] = 'description'
+        # else:
+        #     lstResult = tv(resourceId = 'com.xiaomi.voicecontrol:id/lstResult')
+        #     result_count = lstResult.childCount
+        #     res_tit_list = []
+        #     for t in range(result_count):
+        #         print t,
+        #         res_tit = lstResult.child(resourceId = 'com.xiaomi.voicecontrol:id/titleBar',instance = t).child(resourceId = 'com.xiaomi.voicecontrol:id/title').text
+        #         print res_tit,
+        #         res_tit_list.append(res_tit)
+        #     dict_w['title'] = res_tit_list
+        #     dict_w['tag'] = 'result_list'
+        #     dict_w['recognition'] = tv(resourceId = 'com.xiaomi.voicecontrol:id/title').text
+        #     dict_w['result_msg'] = tv(resourceId = 'com.xiaomi.voicecontrol:id/txtTitle').text
+        tv.dump("%s.xml"%(work_path + os.sep + result_path + os.sep + no))
+        screencapAndPullOut(no, work_path + os.sep + result_path)
+        tv.press('back')
+        print "Done."
+        print "*-*-*-*-*-*-*-*-*-*-*-*-*-*-"
     except:
-        ws.write(i, 0, query)
-
-wb.save('xiaomi_voicesearch_%s.xls'%time.strftime("%Y-%m-%d_%H-%M-%S",time.localtime(time.time())))
+        # dict_w['tag'] = 'occur_err'
+        print "occur_err"
+        pass
+    # f_result.write(str(dict_w).encode('utf-8'))
+    query = f_query.readline()
+    
+f_result.close()
